2c53cb9720069b9322cad431072056c672832358
Phasathorn Suwansri <arbacuz@gmail.com>
2017-02-12 14:04:22 +0700

    update

--- app.py
+++ app.py
@@ -52,8 +52,56 @@ def messenger_webhook():
 @app.post('/ram')
 def get_ram():
     data = request.json
-    print data
-    return None
+    ret = {}
+    ret = {
+         "messages": [
+            {
+              "attachment":{
+                "type":"template",
+                "payload":{
+                  "template_type":"generic",
+                  "elements":[
+                    {
+                      "title":"Classic White T-Shirt",
+                      "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
+                      "subtitle":"Soft white cotton t-shirt is back in style",
+                      "buttons":[
+                        {
+                          "type":"web_url",
+                          "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
+                          "title":"View Item"
+                        },
+                        {
+                          "type":"web_url",
+                          "url":"https://petersapparel.parseapp.com/buy_item?item_id=100",
+                          "title":"Buy Item"
+                        }
+                      ]
+                    },
+                    {
+                      "title":"Classic Grey T-Shirt",
+                      "image_url":"http://petersapparel.parseapp.com/img/item101-thumb.png",
+                      "subtitle":"Soft gray cotton t-shirt is back in style",
+                      "buttons":[
+                        {
+                          "type":"web_url",
+                          "url":"https://petersapparel.parseapp.com/view_item?item_id=101",
+                          "title":"View Item"
+                        },
+                        {
+                          "type":"web_url",
+                          "url":"https://petersapparel.parseapp.com/buy_item?item_id=101",
+                          "title":"Buy Item"
+                        }
+                      ]
+                    }
+                  ]
+                }
+              }
+            }
+          ]
+        }
+    return ret
 
 # Facebook Messenger POST Webhook
 @app.post('/webhook')
