488d358998b8416134395c57c0c8d9829ef399c2
Phasathorn Suwansri <arbacuz@gmail.com>
2017-02-23 14:49:25 +0700

    update track by courier

--- app.py
+++ app.py
@@ -142,6 +142,70 @@ def get_tracking(tracking_id):
     time = datetime.find('div',{'class':'hint'}).get_text()
     return {"place": place, "date":date, "time":time, "tag":tag, "tag_th" :tag_th}
 
+@app.route('/tracking_by_courier', methods=["GET"])
+def tracking_by_courier():
+    courier_link = request.args.get('courier_link')
+    status = get_tracking_by_courier(courier_link)
+    print status
+    if status == None or status == 1:
+        message = {
+            "messages": [
+                {"text": u"เอ หาไม่เจอเลย บอกผิดรึเปล่าน้า?"}
+            ]
+        }
+    elif status == 0:
+        message = {
+            "messages": [
+                {"text": u"พัสดุอยู่ในสถานะ Pending นะ ตอนนี้ Tracky กำลังติดต่อให้อยู่ รออีกสักพัก กลับมาเช็คใหม่นะครับ"}
+            ]
+        }
+    else:
+        if status['tag'] == "Delivered":
+          message = {
+              "messages": [
+                  {"text": u"พัสดุถึงที่หมายแล้ว"},
+                  {"text": status['place']},
+                  {"text": u"เวลา: " + status['date'] + " " + status['time']}
+              ]
+          }
+        else:
+          message = {
+              "messages": [
+                  {"text": u"สถานะ: " + status['tag'] + " (" + status['tag_th'] + ")" },
+                  {"text": status['place']},
+                  {"text": u"เวลา: " + status['date'] + " " + status['time']}
+              ]
+          }
+    print message
+    return jsonify(message)
+
+def get_tracking_by_courier(courier_link)
+    url = courier_link
+    r = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data)
+    recent = soup.find_all('li',{'class':'checkpoint'})
+    if len(recent) <= 0:
+        status_text = soup.find('p',{'id':'status-text'})
+        print status_text
+        if status_text:
+            return 0
+        return None
+    recent = recent[0]
+    place = recent.find('div',{'class':'checkpoint__content'}).find('div',{'class':'hint'}).get_text()
+    datetime = recent.find('div',{'class':'checkpoint__time'})
+    date = datetime.find('strong').get_text()
+    tag = soup.find('p',{'class':'tag'}).get_text()
+    time = datetime.find('div',{'class':'hint'}).get_text()
+    if tag == "In Transit":
+        tag_th = u"กำลังส่ง"
+    elif tag == "Delivered":
+        tag_th = u"ถึงที่หมาย"
+    elif tag == "Out For Delivery":
+        tag_th = u"กำลังจำหน่าย ตามบ้าน"
+    time = datetime.find('div',{'class':'hint'}).get_text()
+    return {"place": place, "date":date, "time":time, "tag":tag, "tag_th" :tag_th}
+
 
 @app.route('/tracking_all', methods=["GET"])
 def tracking_all():
@@ -159,7 +223,6 @@ def tracking_all():
                     {
                       "set_attributes": 
                         {
-                          "courier": courier['name'],
                           "courier_link": courier['link']
                         },
                       "type": "show_block",
@@ -183,7 +246,7 @@ def tracking_all():
               ]
             }
         return jsonify(message)
-        
+
     if status == None or status == 1:
         message = {
             "messages": [
@@ -249,5 +312,6 @@ def get_tracking_all(tracking_id):
         tag_th = u"กำลังจำหน่าย ส่งตามบ้าน"
     time = datetime.find('div',{'class':'hint'}).get_text()
     return {"courier": courier, "place": place, "date":date, "time":time, "tag":tag, "tag_th" :tag_th}
+
 if __name__ == '__main__':
     app.run(debug=True)
